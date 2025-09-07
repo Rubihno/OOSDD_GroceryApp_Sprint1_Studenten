6e826d67351e8647ff56c5d23f60ea5f983f9a4d
Rubihno <rubenhakvoort@gmail.com>
2025-09-07 22:37:23 +0200

    - CreateWindow method toegevoegd om size van de window aan te kunnen passen - Testen of de aangepaste .gitignore werkt

--- Grocery.App/App.xaml.cs
+++ Grocery.App/App.xaml.cs
@@ -1,12 +1,18 @@
-﻿namespace Grocery.App
+﻿
+namespace Grocery.App
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
-            MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState) =>
+            new Window(new AppShell())
+            {
+                Width = 1280,
+                Height = 720
+            };
     }
 }
